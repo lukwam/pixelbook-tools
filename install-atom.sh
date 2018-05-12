cb425f7edd3a76598a4a76f143a9b514fd21c441
Lukas Karlsson <karlsson@broadinstitute.org>
2018-05-12 18:14:33 +0000

    Added atom installer.

---
+++ install-atom.sh
@@ -0,0 +1,9 @@
+#/bin/sh
+
+# add repository
+curl -L https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+sudo apt-get update
+
+# install atom
+sudo apt-get install atom
